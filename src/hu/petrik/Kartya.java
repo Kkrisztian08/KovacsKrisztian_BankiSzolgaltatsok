ac780adc7bf8f38ff9f7d08cdc35e5f1bf73b3c4
Kovács Krisztián <k.krisztian0830@gmail.com>
2021-10-27 11:13:21 +0200

    Kártya osztály funciói

--- src/hu/petrik/Kartya.java
+++ src/hu/petrik/Kartya.java
@@ -1,4 +1,20 @@
 package hu.petrik;
 
-public class Kartya {
+public class Kartya extends BankiSzolgaltatas {
+    private Szamla szamla;
+    private final String kartyaSzam;
+
+    public Kartya(Tulajdonos tulajdonos, Szamla szamla, String kartyaSzam) {
+        super(tulajdonos);
+        this.szamla = szamla;
+        this.kartyaSzam = kartyaSzam;
+    }
+
+    public String getKartyaSzam() {
+        return kartyaSzam;
+    }
+
+    public boolean Vasarlas(int osszeg){
+        return szamla.Kivesz(osszeg);
+    }
 }
