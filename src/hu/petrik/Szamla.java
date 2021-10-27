398bd1524ba2fefcd3a5077096fd3968df00b6c6
Kovács Krisztián <k.krisztian0830@gmail.com>
2021-10-27 10:13:32 +0200

    Számla osztály funciói

--- src/hu/petrik/Szamla.java
+++ src/hu/petrik/Szamla.java
@@ -1,4 +1,25 @@
 package hu.petrik;
 
-public class Szamla {
+public class Szamla extends BankiSzolgaltatas{
+    private int aktualisEgyenleg;
+
+    public Szamla(Tulajdonos tulajdonos, int aktualisEgyenleg) {
+        super(tulajdonos);
+        this.aktualisEgyenleg = aktualisEgyenleg;
+    }
+
+    public int getAktualisEgyenleg() {
+        return aktualisEgyenleg;
+    }
+
+    public void Befizet(int osszeg){
+        aktualisEgyenleg+=osszeg;
+    }
+
+    public final boolean Kivesz(int osszeg){
+        aktualisEgyenleg-=osszeg;
+        return true;
+    }
+
+
 }
